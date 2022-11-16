b9f1e21bcbd6b468345cf310b129dfe2615b781a
Cha Vue <chavue09@gmail.com>
2022-11-16 02:47:19 -0600

    Schema, resolvers and typeDefs created 21.1.6

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,37 @@
+const { User, Thought } = require('../models');
+
+const resolvers = {
+    Query: {
+
+        // get thoughts
+        thoughts: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Thought.find(params).sort({ createdAt: -1 });
+        },
+
+        // get thoughts by id
+        thought: async (parent, { _id }) => {
+            return Thought.findOne({ _id });
+        },
+
+        // get all users
+        users: async () => {
+            return User.find()
+                // .select('__v -password) does not work on my pc
+                .populate('friends')
+                .populate('thoughts');
+        },
+
+        // get a user by username
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                // .select('-__v -password') does not work on my pc
+                .populate('friends')
+                .populate('thoughts');
+        },
+
+
+    }
+};
+
+module.exports = resolvers;
